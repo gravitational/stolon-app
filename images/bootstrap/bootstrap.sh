e0556ea72ec9ed0734475aee9d04c1118022dd04
Sergei Antipov <greendayonfire@gmail.com>
2017-01-24 01:10:44 +0700

    Added node selector for resources.
    Scale sentinels to 3 for 3x and more keeper nodes.

--- images/bootstrap/bootstrap.sh
+++ images/bootstrap/bootstrap.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 /usr/local/bin/stolonboot -sentinels 1 -rpc 1
+
+if [[ $(kubectl get nodes -l stolon-keeper=yes -o name | wc -l) -eq 3 ]]
+then
+    kubectl scale --replicas=3 deployment stolon-sentinel
+fi
