852a0cdaeeda04d611998ad44ddc8bed2c86590f
Sergei Antipov <greendayonfire@gmail.com>
2017-01-24 01:31:00 +0700

    Updated condition

--- images/bootstrap/bootstrap.sh
+++ images/bootstrap/bootstrap.sh
@@ -2,7 +2,7 @@
 
 /usr/local/bin/stolonboot -sentinels 1 -rpc 1
 
-if [[ $(kubectl get nodes -l stolon-keeper=yes -o name | wc -l) -eq 3 ]]
+if [[ $(kubectl get nodes -l stolon-keeper=yes -o name | wc -l) -ge 3 ]]
 then
     kubectl scale --replicas=3 deployment stolon-sentinel
 fi
