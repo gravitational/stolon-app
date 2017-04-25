705c08718852fbd4f02813a8c901682ed22bf752
Jeff Zellner <jeffz@eml.cc>
2017-04-25 11:25:02 -0600

    fix telegraf-node

--- images/telegraf-node/rootfs/usr/local/bin/run.sh
+++ images/telegraf-node/rootfs/usr/local/bin/run.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 # setup and start telegraf
-echo "  address = \"host=stolon-postgres.default.svc user=${STOLON_USERNAME} password=$(cat $STOLON_PASSWORDFILE) sslmode=disable dbname=postgres\"" \
-	 >> /etc/telegraf/telegraf.conf
 /usr/bin/telegraf --quiet --config /etc/telegraf/telegraf.conf
