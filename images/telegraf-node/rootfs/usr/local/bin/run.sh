1e1cd3e66b40f009008d1f032ef2df72e5e03ba1
Jeff Zellner <jeffz@eml.cc>
2017-04-25 12:14:16 -0600

    updates, log telegraf

--- images/telegraf-node/rootfs/usr/local/bin/run.sh
+++ images/telegraf-node/rootfs/usr/local/bin/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # setup and start telegraf
-/usr/bin/telegraf --quiet --config /etc/telegraf/telegraf.conf
+/usr/bin/telegraf --config /etc/telegraf/telegraf.conf
--- images/telegraf/rootfs/usr/local/bin/run.sh
+++ images/telegraf/rootfs/usr/local/bin/run.sh
@@ -3,4 +3,4 @@
 # setup and start telegraf
 echo "  address = \"host=stolon-postgres.default.svc user=${STOLON_USERNAME} password=$(cat $STOLON_PASSWORDFILE) sslmode=disable dbname=postgres\"" \
 	 >> /etc/telegraf/telegraf.conf
-/usr/bin/telegraf --quiet --config /etc/telegraf/telegraf.conf
+/usr/bin/telegraf --config /etc/telegraf/telegraf.conf
