64db529951f0f3f235c6b118bc20b293a2676585
Sergei Antipov <greendayonfire@gmail.com>
2017-08-23 04:40:35 +0700

    Latest update fixes

--- images/hook/entrypoint.sh
+++ images/hook/entrypoint.sh
@@ -13,6 +13,7 @@ if [ $1 = "update" ]; then
     rig delete deployments/stolon-proxy --force
     rig delete deployments/stolon-rpc --force
     rig delete deployments/stolon-sentinel --force
+    rig delete deployments/stolon-utils --force
 
     # wait for keeper pods to go away
     while kubectl get pods --show-all|grep -q stolon-keeper
@@ -31,6 +32,11 @@ if [ $1 = "update" ]; then
     rig upsert -f /var/lib/gravity/resources/sentinel.yaml --debug
     rig upsert -f /var/lib/gravity/resources/utils.yaml --debug
     rig upsert -f /var/lib/gravity/resources/alerts.yaml --debug
+
+    if [[ $(kubectl get nodes -l stolon-keeper=yes -o name | wc -l) -ge 3 ]]
+    then
+        kubectl scale --replicas=3 deployment stolon-sentinel
+    fi
 
     echo "Checking status"
     rig status $RIG_CHANGESET --retry-attempts=120 --retry-period=1s --debug
