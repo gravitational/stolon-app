cdbc3e1de077e2ec2062fbcfe90311a849dffeb6
Sergei Antipov <greendayonfire@gmail.com>
2017-11-29 17:56:56 +0700

    Fix scale condition

--- images/bootstrap/bootstrap.sh
+++ images/bootstrap/bootstrap.sh
@@ -2,9 +2,9 @@
 
 /usr/local/bin/stolonboot -sentinels 1 -rpc 1
 
-if [[ $(kubectl get nodes -l stolon-keeper=yes -o name | wc -l) -ge 3 ]]
+if [ $(kubectl get nodes -l stolon-keeper=yes -o name | wc -l) -ge 3 ]
 then
     kubectl scale --replicas=3 deployment stolon-sentinel
 fi
 
-/opt/bin/kubectl create -f /var/lib/gravity/resources/alerts.yaml
+kubectl create -f /var/lib/gravity/resources/alerts.yaml
