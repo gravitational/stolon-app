d982c0b3834a62e712f3f10d03407ab4cdca080f
Sergei Antipov <greendayonfire@gmail.com>
2018-10-19 13:15:20 -0400

    Fix fo jobs

--- images/jobs/keeper-upgrade.sh
+++ images/jobs/keeper-upgrade.sh
@@ -2,7 +2,7 @@
 # -*- mode: sh; -*-
 
 # File: entrypoint.sh
-# Time-stamp: <2018-05-22 17:05:45>
+# Time-stamp: <2018-10-19 12:56:49>
 # Copyright (C) 2018 Gravitational Inc
 # Description:
 
@@ -24,17 +24,17 @@ cp -R /etc/ssl/cluster-default /etc/ssl/cluster-default-postgres
 chown -R stolon /etc/ssl/cluster-default-postgres
 chmod 0600 /etc/ssl/cluster-default-postgres/default-server-key.pem
 
-gosu stolon mkdir -p /stolon-data/postgres-new /stolon-data/upgrade-state
+mkdir -p /stolon-data/postgres-new /stolon-data/upgrade-state
 cd /stolon-data/upgrade-state
 if [ ! -f /stolon-data/postgres-new/postgresql.conf ]; then
-    PGDATA=/stolon-data/postgres-new gosu stolon ${PGBINNEW}/initdb
+    PGDATA=/stolon-data/postgres-new ${PGBINNEW}/initdb
 fi
 if [ ! -f /stolon-data/upgrade-state/delete_old_cluster.sh ]; then
-    gosu stolon ${PGBINNEW}/pg_upgrade -d /stolon-data/postgres -D /stolon-data/postgres-new
-    gosu stolon cp /stolon-data/postgres/pg_hba.conf /stolon-data/postgres-new/pg_hba.conf
-    gosu stolon rsync -av /stolon-data/postgres/conf.d /stolon-data/postgres-new/
-    gosu stolon rsync -av /stolon-data/postgres/postgresql-base.conf /stolon-data/postgres-new/
+    ${PGBINNEW}/pg_upgrade -d /stolon-data/postgres -D /stolon-data/postgres-new
+    cp /stolon-data/postgres/pg_hba.conf /stolon-data/postgres-new/pg_hba.conf
+    rsync -av /stolon-data/postgres/conf.d /stolon-data/postgres-new/
+    rsync -av /stolon-data/postgres/postgresql-base.conf /stolon-data/postgres-new/
     if [ -d /stolon-data/postgres-old-$PG_VERSION_OLD ]; then rm -rf /stolon-data/postgres-old-$PG_VERSION_OLD; fi
-    gosu stolon mv /stolon-data/postgres /stolon-data/postgres-old-$PG_VERSION_OLD
-    gosu stolon mv /stolon-data/postgres-new /stolon-data/postgres
+    mv /stolon-data/postgres /stolon-data/postgres-old-$PG_VERSION_OLD
+    mv /stolon-data/postgres-new /stolon-data/postgres
 fi
