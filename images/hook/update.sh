feeb325b12eb24644a9483c406e9a75b06ea0aa4
Sergei Antipov <greendayonfire@gmail.com>
2019-12-06 15:11:00 -0500

    Working installation and upgrade

---
+++ images/hook/update.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# -*- mode: bash; -*-
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# check for existence of stolon helm release
+if [[ $(helm list stolon | wc -l) -eq 0 ]]
+then
+    echo "Saving pre-helm resources in changeset: $RIG_CHANGESET"
+    rig cs delete --force -c cs/$RIG_CHANGESET
+    rig delete ds/stolon-keeper --force
+    rig delete deployments/stolon-proxy --force
+    rig delete deployments/stolon-rpc --force
+    rig delete deployments/stolon-sentinel --force
+    rig delete deployments/stolon-utils --force
+    rig delete deployments/stolonctl --force
+    rig delete configmaps/stolon-telegraf --force
+    rig delete configmaps/stolon-telegraf-node --force
+    rig delete configmaps/stolon-pgbouncer --force
+    rig delete secrets/telegraf-influxdb-creds --force
+    rig delete services/stolon-postgres --force
+    rig delete services/stolonctl --force
+    rig delete services/stolon-rpc --force
+    rig delete serviceaccounts/stolon-keeper --force
+    rig delete serviceaccounts/stolon-rpc --force
+    rig delete serviceaccounts/stolon-sentinel --force
+    rig delete serviceaccounts/stolon-utils --force
+    rig delete roles/stolon-keeper --force
+    rig delete roles/stolon-rpc --force
+    rig delete roles/stolon-sentinel --force
+    rig delete roles/stolon-utils --force
+    rig delete rolebindings/stolon-keeper --force
+    rig delete rolebindings/stolon-rpc --force
+    rig delete rolebindings/stolon-sentinel --force
+    rig delete rolebindings/stolon-utils --force
+    rig freeze
+fi
+
+helm upgrade --install stolon /var/lib/gravity/resources/charts/stolon \
+     --values /var/lib/gravity/resources/custom-values.yaml \
+     --set existingSecret=stolon
---
+++ images/stolon/rootfs/usr/local/bin/update-secret.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+if ! kubectl get secrets $SECRET_NAME -o json | jq '.data.pg_repl_password' -e
+then
+    echo "Adding key pg_repl_password into stolon secret"
+    kubectl get secrets $SECRET_NAME -o json | jq ".data[\"pg_repl_password\"]=\"$PG_REPL_PASSWORD\"" | kubectl apply -f -
+fi
+/* vim: set filetype=sh : */ 
