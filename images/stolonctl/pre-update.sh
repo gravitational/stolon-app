8fde4333a1711670447197b0f4a71328a15d4632
Sergei Antipov <sergei@gravitational.com>
2020-08-10 15:44:59 -0400

    Fix doubled kubectl for pre-update hook

--- images/stolonctl/pre-update.sh
+++ images/stolonctl/pre-update.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # scale down sentinel replicas to avoid master failover during planet upgrade
 # and potentially having multi-master situation
-/usr/bin/kubectl scale --replicas=0 deployment stolon-sentinel
+kubectl scale --replicas=0 deployment stolon-sentinel
 
 # check cluster health before starting an upgrade to eliminate any possible issue with that
-/usr/bin/stolonctl status --short
+stolonctl status --short
