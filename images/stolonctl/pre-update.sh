25f08a872b56aa4de0506e88fb0331c187189c65
Sergei Antipov <sergei@gravitational.com>
2020-08-10 18:32:54 -0400

    Scale down sentinels after health check

--- images/stolonctl/pre-update.sh
+++ images/stolonctl/pre-update.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -x
 
+# check cluster health before starting an upgrade to eliminate any possible issue with that
+stolonctl status --short
+
 # scale down sentinel replicas to avoid master failover during planet upgrade
 # and potentially having multi-master situation
 kubectl scale --replicas=0 deployment stolon-sentinel
-
-# check cluster health before starting an upgrade to eliminate any possible issue with that
-stolonctl status --short
