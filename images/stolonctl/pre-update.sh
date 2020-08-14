adcb16aa1f66d8fd6bb56f9a6604c4b3f2dc3bd9
Sergei Antipov <sergei@gravitational.com>
2020-08-13 20:51:48 -0400

    More changes in the process

--- images/stolonctl/pre-update.sh
+++ images/stolonctl/pre-update.sh
@@ -7,3 +7,4 @@ stolonctl status --short
 # scale down sentinel replicas to avoid master failover during planet upgrade
 # and potentially having multi-master situation
 kubectl scale --replicas=0 deployment stolon-sentinel
+kubectl patch daemonset stolon-keeper -p '{"spec": {"template": {"spec": {"nodeSelector": {"non-existing": "true"}}}}}'
