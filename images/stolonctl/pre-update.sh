2c572e1c2d59e45abf412dad663bed4bee6d81bc
mohammedabdulwahhab <furkhan324@berkeley.edu>
2020-11-09 10:07:56 -0800

    Changes to re-enable Telegraf (Compatible with Prometheus) (#177)
    * this should work
    
    * Fixes for service monitor
    
    * Update gravity versions
    
    * Delete alert configmap
    
    * Delete gravity alert resource
    
    * Do not use gravity volume
    
    * Delete true to see the error
    
    * Add loop for cluster health check
    
    * Delete /opt/bin
    
    * Make output for stolonctl status visible
    
    * Update labels
    
    * Add POD_IP variable
    
    Co-authored-by: Sergei Antipov <sergei@gravitational.com>

--- images/stolonctl/pre-update.sh
+++ images/stolonctl/pre-update.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
-set -x
+set -o errexit
+set -o xtrace
 
 # check cluster health before starting an upgrade to eliminate any possible issue with that
-stolonctl status --short
+until stolonctl status --short
+do
+	sleep 10
+done
 
 # scale down sentinel replicas to avoid master failover during planet upgrade
 # and potentially having multi-master situation
 kubectl scale --replicas=0 deployment stolon-sentinel
+
+# delete alerts resource to fix the issue with prometheus
+/opt/bin/gravity resource rm alert stolon-replication-lag
+
 kubectl patch daemonset stolon-keeper -p '{"spec": {"template": {"spec": {"nodeSelector": {"non-existing": "true"}}}}}'
