41eb35be3b4604e580808609d56fb85cc34d20a3
Sergei Antipov <sergei@gravitational.com>
2020-12-17 13:03:08 -0500

    Replace bash with sh

--- resources/install.sh
+++ resources/install.sh
@@ -15,5 +15,5 @@ set +e
 
 set -e
 
-timeout 5m bash -c "while ! kubectl get job stolon-postgres-hardening; do sleep 10; done"
+timeout 5m sh -c "while ! /usr/local/bin/kubectl get job stolon-postgres-hardening; do sleep 10; done"
 /usr/local/bin/kubectl wait --for=condition=complete --timeout=5m job/stolon-postgres-hardening
