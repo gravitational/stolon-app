22dd64c868800402aa9559888a87d58981669072
Walt Della <walt@gravitational.com>
2021-01-11 12:06:56 -0800

    Remove bash-isms from version.sh.
    This allows it to run under generic POSIX shells, such as alpine's ash.
    This is useful when building in a container.  Immediately, this change
    enables Drone CI.
    
    (cherry picked from commit e265b2fb810de7d7d30e6bac0c5aa7d291108087)

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # this versioning algo:
 # keeps tag as is in case if this version is an equal match
@@ -9,10 +9,10 @@ LONG_TAG=`git describe --tags`
 COMMIT_WITH_LAST_TAG=`git show-ref --tags --dereference | grep "refs/tags/${SHORT_TAG}^{}" | awk '{print $1}'`
 COMMITS_SINCE_LAST_TAG=`git rev-list  ${COMMIT_WITH_LAST_TAG}..HEAD --count`
 
-if [[ "$LONG_TAG" == "$SHORT_TAG" ]] ; then
+if [ "$LONG_TAG" = "$SHORT_TAG" ] ; then  # the current commit is tagged as a release
     echo "$SHORT_TAG"
-elif [[ "$SHORT_TAG" != *-* ]] ; then
+elif echo "$SHORT_TAG" | grep -Eq ".*-.*"; then  # the current commit is a descendant of a pre-release version (e.g. rc, alpha, or beta)
     echo "$SHORT_TAG-${COMMITS_SINCE_LAST_TAG}"
-else
+else   # the current commit is a descendant of a regular version
     echo "$SHORT_TAG.${COMMITS_SINCE_LAST_TAG}"
 fi
