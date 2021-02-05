d755d2abb5d2e9911312f4c57ff65524b77e777b
Sergei Antipov <sergei@gravitational.com>
2021-02-05 14:54:40 -0500

    Add error handling for telegraf secret copy job

--- images/telegraf/copy-secret.sh
+++ images/telegraf/copy-secret.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+set -o nounset
+set -o errexit
+set -o pipefail
 
 ## copy telegraf secret from monitoring namespace
 if kubectl --namespace=monitoring get secret telegraf-influxdb-creds >/dev/null 2>&1
 then
     kubectl --namespace=monitoring get secret telegraf-influxdb-creds -o yaml |\
-        sed 's/namespace: monitoring/namespace: default/' | kubectl --namespace=default apply -f -
+        sed 's/namespace: monitoring/namespace: default/' | kubectl --namespace=default create -f -
 fi
